f124312122b8e8a5f69e594c4174630fe26bb27a
Mrsolti <140240982+Mrsolti@users.noreply.github.com>
2024-04-04 20:42:23 +0545

    Add files via upload

---
+++ hotel booking/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Hotel Booking</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+   
+    
+    <style>
+        .well
+        {
+            background: rgba(0,0,0,0.7);
+            border: none;
+    
+        }
+        .wellfix
+        {
+            background: rgba(0,0,0,0.7);
+            border: none;
+            height: 60px;
+        }
+		body
+		{
+			background-image: url("images/ht.jpg");
+			background-repeat: no-repeat;
+			background-attachment: fixed;
+		}
+		p
+		{
+			font-size: 13px;
+		}
+        .pro_pic
+        {
+            border-radius: 50%;
+            height: 50px;
+            width: 50px;
+            margin-bottom: 15px;
+            margin-right: 15px;
+        }
+		
+    </style>
+    
+    
+</head>
+
+<body>
+    <div class="container">
+      
+      
+       <img class="img-responsive" src="images/home_banner.jpg" style="width:100%; height:180px;">      
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="room.php">Room &amp; Facilities</a></li>
+                    <li><a href="reservation.php">Online Reservation</a></li>
+                    <li><a href="admin.php">Admin</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="http://www.facebook.com"><img src="images/facebook.png"></a></li>
+                    <li><a href="http://www.twitter.com"><img src="images/twitter.png"></a></li>                    
+                </ul>
+            </div>
+        </nav>
+
+     
+        <div class="jumbotron">
+        <div class="w3-content w3-section">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/1.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images\home_gallary\506 room.jpg" style="width:100%; height:450px;">
+            
+        </div>    
+        </div>
+        <hr>
+        <div class="row" style="color: #ed9e21">
+            <div class="col-md-12 well" >
+              <h4><strong style="color: #ffbb2b">About</strong></h4><br>
+              <p>Online hotel reservations are a popular method for booking hotel rooms. Travelers can book rooms on a computer by using online security to protect their privacy and financial information and by using several online travel agents to compare prices and facilities at different hotels</p>
+              <br>
+              <p>We Hotel president members and owners will request the service user to choose us as your desitine stay place as we can provide u with the various type of sepcial facielites as we can help u chose the various advanterous activities around the pokhara valley</p>
+              <br>
+              <p>some of the advanture activities we vcan provide our servise users are bunjey jumping, jeep flies ,paragliding and swoo onnn.</p>
+              
+            </div>  
+        </div>
+        <div class="row" style="color: #ffbb2b">
+            <div class="col-md-4 wellfix">
+              <h4><strong>Contact Us</strong></h4><hr>
+              Address : Lakeside,Street No-19<br>
+              E-Mail : sabeenadhikari74@gmail.com <br>
+            </div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4 wellfix">
+                <h4><strong>Developed By</strong></h4><hr>
+                <a href="#">Sabin Adhikari</a>
+
+            </div>
+        </div>
+        
+
+
+
+    </div>
+    
+    
+    
+    
+    
+
+
+
+
+    <script src="my_js/slide.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
